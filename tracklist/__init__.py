d557ceb906073519b5cc4d0212db12e87fa09595
fwcd <fwcdmail@gmail.com>
2022-10-03 23:24:11 +0200

    Use Path where possible

--- tracklist/__init__.py
+++ tracklist/__init__.py
@@ -24,41 +24,41 @@ _TRANSFORMS = {
 def _file_format(path: Path) -> Optional[Format]:
     return _FORMATS.get(path.suffix.split('.')[-1], None)
 
-def _open(file_like: str, mode: str):
-    if file_like == '-':
+def _open(path: Path, mode: str):
+    if str(path) == '-':
         if 'r' in mode:
             return nullcontext(sys.stdin)
         else:
             return nullcontext(sys.stdout)
     else:
-        return open(file_like, mode)
+        return open(path, mode)
 
-def _read_inputs(paths: list[str]) -> list[Tracklist]:
+def _read_inputs(paths: list[Path]) -> list[Tracklist]:
     inputs: list[Tracklist] = []
 
     for path in paths:
-        format = _file_format(Path(path))
+        format = _file_format(path)
         if format is None:
             print(f"Format of {path} was not recognized, the following are supported: {', '.join(sorted(_FORMATS.keys()))}")
             sys.exit(1)
 
         with _open(path, 'r') as f:
             tracklist = format.parse(f.read())
-            tracklist = resolve_duration(tracklist, Path(path).resolve().parent)
+            tracklist = resolve_duration(tracklist, path.resolve().parent)
             inputs.append(tracklist)
     
     return inputs
 
-def _write_output(tracklist: Tracklist, format: Format, path: Optional[str]=None):
-    with _open(path or '-', 'w') as f:
+def _write_output(tracklist: Tracklist, format: Format, path: Optional[Path]=None):
+    with _open(path or Path('-'), 'w') as f:
         f.write(format.format(tracklist) + '\n')
 
 def main():
     parser = argparse.ArgumentParser(description='Tracklist processor')
-    parser.add_argument('-o', '--output', help='The output file.')
+    parser.add_argument('-o', '--output', type=Path, help='The output file.')
     parser.add_argument('-f', '--format', choices=sorted(_FORMATS.keys()), help='The output format. Defaults to the format determined by the extension of the --output files and, if none are specified, to cuesheets.')
     parser.add_argument('transform', choices=sorted(_TRANSFORMS.keys()), help='The transform to perform.')
-    parser.add_argument('inputs', nargs='+', help='The input files.')
+    parser.add_argument('inputs', type=Path, nargs='+', help='The input files.')
 
     args = parser.parse_args()
     input_paths = args.inputs
