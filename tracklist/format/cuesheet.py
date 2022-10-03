1ff1a08472ba45d98467fe401493e50959a4e7e8
fwcd <fwcdmail@gmail.com>
2022-10-03 23:00:49 +0200

    Fix cuesheet parser and formatter

--- tracklist/format/cuesheet.py
+++ tracklist/format/cuesheet.py
@@ -9,11 +9,11 @@ def _parse_time(raw: str) -> int:
     return 60 * minutes + seconds
 
 def _format_time(total_seconds: int) -> str:
-    minutes = total_seconds / 60
+    minutes = total_seconds // 60
     seconds = total_seconds % 60
     return f'{minutes:02}:{seconds:02}:00'
 
-_FILE_PATTERN = re.compile(r'^FILE\s+"(?P<file>[^"]*)"\s+(\w+)')
+_FILE_PATTERN = re.compile(r'^FILE\s+"(?P<file>[^"]*)"\s+(?P<format>\w+)')
 _TRACK_PATTERN = re.compile(r'^TRACK\s+(?P<index>\d+)\s+(?P<type>\S+)')
 _TITLE_PATTERN = re.compile(r'^TITLE\s+"(?P<title>[^"]*)"')
 _PERFORMER_PATTERN = re.compile(r'^PERFORMER\s+"(?P<performer>[^"]*)"')
@@ -29,25 +29,25 @@ class CuesheetFormat(Format):
         for line in raw.splitlines():
             line = line.strip()
             if line:
-                if entry:
+                if match := _FILE_PATTERN.search(line):
+                    tracklist.file = match['file']
+                    tracklist.format = match['format']
+                elif match := _TRACK_PATTERN.search(line):
+                    if match['type'] == 'AUDIO':
+                        if entry:
+                            tracklist.entries.append(entry)
+                        entry = TrackEntry()
+                elif entry:
                     if match := _TITLE_PATTERN.search(line):
                         entry.title = match['title']
                     elif match := _PERFORMER_PATTERN.search(line):
                         entry.artist = match['performer']
                     elif match := _INDEX_PATTERN.search(line):
                         entry.start_seconds = _parse_time(match['time'])
-                else:
-                    if match := _FILE_PATTERN.search(line):
-                        tracklist.file = match[1]
-                    elif match := _TRACK_PATTERN.search(line):
-                        if match['type'] == 'AUDIO':
-                            if entry:
-                                tracklist.entries.append(entry)
-                            entry = TrackEntry()
-                    elif match := _TITLE_PATTERN.search(line):
-                        tracklist.title = match['title']
-                    elif match := _PERFORMER_PATTERN.search(line):
-                        tracklist.artist = match['performer']
+                elif match := _TITLE_PATTERN.search(line):
+                    tracklist.title = match['title']
+                elif match := _PERFORMER_PATTERN.search(line):
+                    tracklist.artist = match['performer']
 
         if entry:
             tracklist.entries.append(entry)
